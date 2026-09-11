313c45a42f8ca0271bb3b7d668b5fc877f08b67a
dhisyu78 <yudhisfmx@gmail.com>
2026-09-11 10:00:04 +0700

    percob4

--- jobsheet3/KoperasiDemo.java
+++ jobsheet3/KoperasiDemo.java
@@ -2,7 +2,7 @@ package jobsheet3;
 
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
+        Anggota anggota1 = new Anggota("Iwan Setiawan", "jalan Mawar");
         System.out.println("Simpanan " +anggota1.getNama()+ " : Rp " +anggota1.getSimpanan());
 
         anggota1.setNama("Iwan Setiawan");
